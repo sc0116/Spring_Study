0c42d2e2fbf8f83d5259922e38c52d0a4a4206a4
SeungCheol <ssc6839@gmail.com>
2021-05-02 22:13:05 +0900

    PUT

---
+++ 01_SpringBoot/put/src/main/java/com/example/put/PutApiController.java
@@ -0,0 +1,17 @@
+package com.example.put;
+
+import dto.PutRequestDto;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
+public class PutApiController {
+
+    @PutMapping("/put/{userId}")
+    public PutRequestDto put(@RequestBody PutRequestDto requestDto, @PathVariable(name = "userId") Long id) {
+
+        System.out.println(id);
+        System.out.println(requestDto);
+        return requestDto;
+    }
+}
